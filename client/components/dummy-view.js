cbff1459a56568adb5121fbe1b8d596e102cebaa
Shustoff <exoticfootballcom@gmail.com>
2020-05-04 00:02:28 +0300

    first commit added

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -9,7 +9,7 @@ const Dummy = () => {
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component
+          This is my first commit in the course
         </div>
       </div>
     </div>
